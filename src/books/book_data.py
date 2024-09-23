df955073437cc82942b5964ef2b0f8f7d6e6a9cf
Nam Duong <phamnamduong583@gmail.com>
2024-09-23 19:24:58 +0700

    create model,schema  and routes

---
+++ src/books/book_data.py
@@ -0,0 +1,47 @@
+books =[
+  {
+    "id": 1,
+    "title": "To Kill a Mockingbird",
+    "author": "Harper Lee",
+    "publisher": "J.B. Lippincott & Co.",
+    "published_date": "1960-07-11",
+    "page_count": 281,
+    "language": "English"
+  },
+  {
+    "id": 2,
+    "title": "1984",
+    "author": "George Orwell",
+    "publisher": "Secker & Warburg",
+    "published_date": "1949-06-08",
+    "page_count": 328,
+    "language": "English"
+  },
+  {
+    "id": 3,
+    "title": "Pride and Prejudice",
+    "author": "Jane Austen",
+    "publisher": "T. Egerton",
+    "published_date": "1813-01-28",
+    "page_count": 272,
+    "language": "English"
+  },
+  {
+    "id": 4,
+    "title": "The Lord of the Rings",
+    "author": "J.R.R. Tolkien",
+    "publisher": "George Allen & Unwin",
+    "published_date": "1954-07-29",
+    "page_count": 1216,
+    "language": "English"
+  },
+  {
+    "id": 5,
+    "title": "The Little Prince",
+    "author": "Antoine de Saint-Exupéry",
+    "publisher": "Reynal & Hitchcock",
+    "published_date": "1943-04-06",
+    "page_count": 93,
+    "language": "French"
+  }
+]
